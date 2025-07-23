ded5761d94b39a6c30caf1aa66768fb34d1174ab
Luis Lavado Llaro <luisito.ingenermax@gmail.com>
2025-07-23 10:05:59 -0500

    Data management

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initialProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=11',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=21',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=31',
+        createdAt: new Date().toISOString()
+      }
+    ];
+
+    this.products.set(initialProducts);
+  }
+
   fromChild(event: string): void {
     console.log('We are in the parent component.', event);
   }
