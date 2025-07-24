f0d6b08228d8287f9d02f0e2a486dd5890bbc47a
Luis Lavado Llaro <luisito.ingenermax@gmail.com>
2025-07-24 11:56:02 -0500

    Get Categories

--- documentation/doc-services.md
+++ documentation/doc-services.md
@@ -1,5 +1,6 @@
 ng g s domains/shared/services/cart
 ng g s domains/shared/services/product
+ng g s domains/shared/services/category
 
 
 <!-- directives: Para manipulaciones del DOM, no es muy comun, pero habra escenarios que lo requieran -->
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -7,37 +7,53 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
-    this.productService.getProducts().subscribe({
-      next: (products) => {
-        this.products.set(products)
-      },
-      error: () => {
-        console.log('error');
-      }
-    });
-
-
+    this.getProducts();
+    this.getCategories();
   }
-
-
 
   addToCart(product: Product): void {
     this.cartService.addToCart(product);
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: () => {
+        console.log('error getProducts');
+      }
+    });
+  }
+
+   getCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: () => {
+        console.log('error getCategories');
+      }
+    });
   }
 
 
